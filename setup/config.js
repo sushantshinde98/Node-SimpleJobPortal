fd81e1b19ece6f01814ec2c8e7f041a1f930f82d
Sushant Shinde <55993764+sushantshinde98@users.noreply.github.com>
2023-03-23 19:40:24 +0530

    connectionstring

--- setup/config.js
+++ setup/config.js
@@ -1,5 +1,5 @@
 const myconfig={
-    mongoURL:`mongodb+srv://${process.env.mDB_USER}:${process.env.mDB_PASS}@jobportalcluster.yn5dh.mongodb.net/${process.env.mDB_NAME}?retryWrites=true&w=majority`,
+    mongoURL:`mongodb+srv://${process.env.mDB_USER}:${process.env.mDB_PASS}@todocluster1.laavd.mongodb.net/${process.env.mDB_NAME}?retryWrites=true&w=majority`,
     nodePort:3000,
 }
 
